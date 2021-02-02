a157f68ba4053f8b1d23a55b298b4ef4131292ec
Jeremy Zhong <Alien668@outlook.com>
2021-02-02 22:02:36 +0800

    Update Annotation

--- Health.py
+++ Health.py
@@ -5,13 +5,14 @@ from lxml import etree
 import json
 import db
 
+
 class Student_health:
     def __init__(self):
         self.username = ''
         self.password = ''
-        self.info = ''  #昨天打卡的信息抓取
+        self.info = ''  # 昨天打卡的信息抓取
         self.login_URL = 'https://cas.gench.edu.cn/cas/login?service=http%3A%2F%2Fi1.gench.edu.cn%2F_web%2Ffusionportal%2Fwelcome.jsp%3F_p%3DYXM9MSZwPTEmbT1OJg__'
-        self.stu_URL = 'http://i1.gench.edu.cn/_web/fusionportal/stu/index.jsp?_p=YXM9MSZwPTEmbT1OJg__'  #最终学生登录页面
+        self.stu_URL = 'http://i1.gench.edu.cn/_web/fusionportal/stu/index.jsp?_p=YXM9MSZwPTEmbT1OJg__'  # 最终学生登录页面
         self.headers = {
             'User-Agent': UserAgent().chrome
         }
@@ -21,8 +22,9 @@ class Student_health:
     '''
     解析登录页面并抓取登录表单需要的参数
     '''
+
     def login_parse(self):
-        #session = requests.Session()
+        # session = requests.Session()
         self.session.headers = self.headers
         try:
             resp = self.session.get(self.login_URL)
@@ -35,13 +37,15 @@ class Student_health:
             }
         except:
             logger.info('学号：' + self.username + '==================>登录出错')
-        #print(self.session.cookies)
+        # print(self.session.cookies)
 
     def getsession(self):
         return self.session
+
     '''
     登入系统
     '''
+
     def login(self):
         # logger.info('进入login方法')
         self.login_form['username'] = self.username
@@ -65,12 +69,13 @@ class Student_health:
     '''
     打卡模块
     '''
+
     def health(self):
         session = self.getsession()
         # session.headers.pop('Referer')
         session.headers['Referer'] = 'http://ihealth.hq.gench.edu.cn/pc/login-student'
         try:
-            session.get('http://ihealth.hq.gench.edu.cn/api/login/student')  #拿到gench_hq_user
+            session.get('http://ihealth.hq.gench.edu.cn/api/login/student')  # 拿到gench_hq_user
             # print(session.cookies)
             # print(session.headers)
             resp = session.post('http://ihealth.hq.gench.edu.cn/api/GDaily/pageuseryestoday')
@@ -81,9 +86,11 @@ class Student_health:
             final = json.loads(resp.text)
             if final['suc']:
                 # logger.info('学号：' + self.info['userid'] + '打卡地点：' + self.info['slocation'] + self.info['location'] + '======>打卡成功!')
-                logger.info('学号：{} 姓名：{} 打卡地点：{}-{} <打卡成功>'.format(self.info['userid'], self.info['username'], self.info['slocation'], self.info['location']))
+                logger.info('学号：{} 姓名：{} 打卡地点：{}-{} <打卡成功>'.format(self.info['userid'], self.info['username'],
+                                                                   self.info['slocation'], self.info['location']))
         except:
             logger.info('学号：' + self.username + '======> <提交信息出错-打卡失败>')
+
 
 if __name__ == '__main__':
 
@@ -98,8 +105,8 @@ if __name__ == '__main__':
     # 可改用多线程实现同时打卡
     for s in infos:
         stu = Student_health()
-        stu.username = s['username']
-        stu.password = s['password']
+        stu.username = s['username']  # 学号
+        stu.password = s['password']  # 密码
 
         stu.login_parse()
         flag = stu.login()
